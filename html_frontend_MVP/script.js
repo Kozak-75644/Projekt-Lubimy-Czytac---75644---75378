b90bd06ee6c7977f34eed35912b651e13c5ec38d
75378 <bmelche1@stu.vistula.edu.pl>
2026-05-07 22:21:33 +0200

    First try to deploy project on Github Pages

--- html_frontend_MVP/script.js
+++ html_frontend_MVP/script.js
@@ -1,5 +1,5 @@
 // server's URL 
-const API_URL = 'example'; 
+const API_URL = 'https://bohdanmelch.pythonanywhere.com/'; 
 
 document.addEventListener('DOMContentLoaded', () => {
     const bookForm = document.getElementById('add-book-form');
